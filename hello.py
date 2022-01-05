240bd172480450fac5a5a6cffcb992fae321d783
Devanand Yadav <devananraj2002@gmail.com>
2022-01-05 11:07:29 +0530

    hello.py added

---
+++ hello.py
@@ -0,0 +1,6 @@
+def welcome():
+    """
+        This is a simple hello world function with no return type.
+    """
+    for i in range(5):
+        print('Hello World! \nWelcome to the world of Artificial Intelligence.')
